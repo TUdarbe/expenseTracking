c40293cf61bd2eee8017ee1ac57961d22ace4ca4
TUdarbe <troyudarbe@hotmail.com>
2022-11-28 17:17:35 -0500

    Adjust form

--- react/components/ExpenseForm.tsx
+++ react/components/ExpenseForm.tsx
@@ -22,6 +22,11 @@ function ExpenseForm() {
             Description
           </label>
           <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+
+          <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
+            Note
+          </label>
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
         </div>
       </div>
     </form>
